d1c40cb4f4e02e7e0aa1e51607fe9c5826e9ffa6
laporcarge <laporcarge@gmail.com>
2025-10-27 23:00:16 -0300

    Delete api.js

--- api.js
+++
@@ -1,7 +0,0 @@
-// api.js
-export default async function handler(req, res) {
-  const url = process.env.TARGET || "https://soporte-swkq.vercel.app/";
-  // Si entras a /r/loquesea → te redirige al TARGET
-  res.writeHead(302, { Location: url });
-  res.end();
-}
